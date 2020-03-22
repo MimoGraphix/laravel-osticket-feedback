9a2301f08888459a564a71ef60d03effb98d0c1a
MimoGraphix <mimographix@gmail.com>
2020-03-22 14:01:56 +0100

    Feedback mutácie

--- resources/lang/sk/feedback.php
+++ resources/lang/sk/feedback.php
@@ -1,5 +1,15 @@
 <?php
 
 return [
-
+    'feedback_button' => 'Feedback',
+    'ticket-created' => 'Feedback bol odoslaný',
+    'message' => 'Test správy',
+    'subject' => 'Predmet',
+    'email' => 'Váš email',
+    'name' => 'Vaše meno',
+    'modal' => [
+        "title" => 'Feedback',
+        "close" => 'zavrieť',
+        "send" => 'Odoslať',
+    ],
 ];
